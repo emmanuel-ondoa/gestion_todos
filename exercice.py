b4ac02442ba313b979d480356713a9432ff713de
Emmanuel Ondoa <emmanuelondoa2006@gmail.com>
2024-11-26 15:17:49 +0100

    ajout de la gestion des statuts avec une errreur volontaire

--- exercice.py
+++ exercice.py
@@ -10,28 +10,48 @@ def creer_todos () :
 def lister_todos() :
         if  not  todos:
           print("aucune tache disponible ")
-        else:       
+        else:
          for index , todo  in enumerate(todos) :
             index=index+1
-            print (f"{index}.{todo['titre']}-{todo['statut']}") 
+            print (f"{index}.{todo['titre']}-{todo['statut']}")
+
+def modifier_statut_todo():
+    lister_todos()
+    index = int(input("Entrez le numéro de la tâche à modifier : ")) - 1
+    if todos[index]['statut'] == "A faire":
+        todos[index]['statut'] = "Fait"
+    else:
+        todos[index]['statut'] = "A fair"  
+    print(f"Statut de la tâche '{todos[index]['titre']}' modifié.")
 
 print ("=======MENU PRINCIPAL=======")
 print ("1.creer un todo")
-print("2.liste des todos")  
-print("3: quitter") 
-choix = int(input()) 
-while choix != 3:
+print("2.liste des todos")
+print("3.modifier le statut d un todo ")
+print("4. quitter")
+choix = int(input())
+while choix != 4:
   if choix==1:
       creer_todos()
       print ("=======MENU PRINCIPAL=======")
       print ("1.creer un todo")
-      print("2.liste des todos")  
-      print("3: quitter") 
+      print("2.liste des todos")
+      print("3.modifier le statut d un todo")
+      print("4: quitter")
       choix = int(input())
-  else:
+  elif choix==2:
       lister_todos()
       print ("=======MENU PRINCIPAL=======")
       print ("1.creer un todo")
-      print("2.liste des todos")  
-      print("3: quitter")
-      choix = int(input()) 
+      print("2.liste des todos")
+      print("3.modifier le statut d un todo")
+      print("4: quitter")
+      choix = int(input())
+  else:
+      modifier_statut_todo()
+      print ("=======MENU PRINCIPAL=======")
+      print ("1.creer un todo")
+      print("2.liste des todos")
+      print("3.modifier le statut d un todo")
+      print("4: quitter")
+      choix = int(input())
